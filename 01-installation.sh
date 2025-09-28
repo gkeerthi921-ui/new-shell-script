e440bab6f18fe36aa56708b2fd1899cbc9521667
gkeerthi921-ui <gkeerthi921@gmail.com>
2025-09-28 15:08:06 +0530

    installation

--- 01-installation.sh
+++ 01-installation.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [USERID -ne 0];then
+if [ "$USERID" -ne 0 ];then
 echo "need root access"
 exit 1
 fi
 dnf install mysql -y
-if [USERID -ne 0];then
+if [ $? -ne 0 ];then
 echo "installation failed"
 exit 1
 else 
